5a616bd18ca85e25099d11b97ebee34f123a9db2
marb61a <marb61a@gmail.com>
2018-01-16 13:56:05 +0000

    Added random number generator, setup dice to use each random number generator when clicking roll

--- app/src/main/java/com/example/martin/android_dice/MainActivity.java
+++ app/src/main/java/com/example/martin/android_dice/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,13 +19,31 @@ public class MainActivity extends AppCompatActivity {
         Button rollButton;
         rollButton = findViewById(R.id.rollButton);
 
-        ImageView leftDice = findViewById(R.id.image_leftDice);
-        ImageView rightDice = findViewById(R.id.image_rightDice);
+        final ImageView leftDice = findViewById(R.id.image_leftDice);
+        final ImageView rightDice = findViewById(R.id.image_rightDice);
+
+        final int [] diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
 
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("Android_Dice", "Hello There");
+
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(6);
+                Log.d("Android_Dice", "The random number generated is " + number);
+
+                leftDice.setImageResource(diceArray[number]);
+
+                number = randomNumberGenerator.nextInt(6);
+                rightDice.setImageResource(diceArray[number]);
             }
         });
     }
