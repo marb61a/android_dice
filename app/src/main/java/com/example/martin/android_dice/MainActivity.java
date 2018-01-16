e4a941b5869057afabe3d9fb3a70032ae5facc9c
marb61a <marb61a@gmail.com>
2018-01-16 13:16:13 +0000

    Added button, image views and an a listener to the main activity

--- app/src/main/java/com/example/martin/android_dice/MainActivity.java
+++ app/src/main/java/com/example/martin/android_dice/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.martin.android_dice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = findViewById(R.id.rollButton);
+
+        ImageView leftDice = findViewById(R.id.image_leftDice);
+        ImageView rightDice = findViewById(R.id.image_rightDice);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Android_Dice", "Hello There");
+            }
+        });
     }
 }
